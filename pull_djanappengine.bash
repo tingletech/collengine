4264584ed41ba366bfe042dfb4879d8057e3a1ac
Ted Lawless <lawlesst@gmail.com>
2011-02-05 10:25:43 -0500

    Minor changes to pull_djanappengine.bash for setup on Mac.

--- pull_djanappengine.bash
+++ pull_djanappengine.bash
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 # automates the steps in http://www.allbuttonspressed.com/projects/djangoappengine
 set -eux
-rm -rf django djangoappengine djangotoolbax django-dbindexer
-curl http://bitbucket.org/wkornewald/django-nonrel/get/tip.tar.gz | tar zxf -
-curl http://bitbucket.org/wkornewald/djangoappengine/get/tip.tar.gz | tar zxf -
-curl http://bitbucket.org/wkornewald/djangotoolbox/get/tip.tar.gz | tar zxf -
-curl http://bitbucket.org/wkornewald/django-dbindexer/get/tip.tar.gz | tar zxf -
+rm -rf django djangoappengine djangotoolbax django-dbindexer dbindexer 
+curl https://bitbucket.org/wkornewald/django-nonrel/get/tip.tar.gz | tar zxf -
+curl https://bitbucket.org/wkornewald/djangoappengine/get/tip.tar.gz | tar zxf -
+curl https://bitbucket.org/wkornewald/djangotoolbox/get/tip.tar.gz | tar zxf -
+curl https://bitbucket.org/wkornewald/django-dbindexer/get/tip.tar.gz | tar zxf -
 mv django-nonrel/django django
 mv djangotoolbox/djangotoolbox djangotoolbox-djangotoolbox
 rm -rf djangotoolbox
 mv djangotoolbox-djangotoolbox djangotoolbox
 mv django-dbindexer/dbindexer dbindexer
-rm -rf django-nonrel djangotoolbox django-dbindexer
 
