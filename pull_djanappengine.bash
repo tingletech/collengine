08d8885a3ab0185a56d3cb7542c42fb4b35ac74c
BRIAN TINGLE <tingle@BRIAN-TINGLEs-MacBook-Air.local>
2011-02-15 17:17:19 -0800

    remove non-english locale to fit under 3000 files on app engine

--- pull_djanappengine.bash
+++ pull_djanappengine.bash
@@ -12,3 +12,12 @@ rm -rf djangotoolbox
 mv djangotoolbox-djangotoolbox djangotoolbox
 mv django-dbindexer/dbindexer dbindexer
 
+# need to fit under 3000 files; remove non-english locales from django
+shopt -s extglob
+for i in `find django -name "locale"`
+do
+  if [ -e $i ]; then
+    mkdir -p $i/xx	# hack, sometimes there is only an "en" directory in locale and the next line would fail
+    rm -rf $i/!(en*)
+  fi
+done
